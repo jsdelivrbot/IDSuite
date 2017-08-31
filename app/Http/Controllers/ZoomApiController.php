97ad22c652894333235ce8a2b607869e991e7a92
Alex Macdonald-Smith <Alex.MacSmith@gmail.com>
2017-08-31 10:53:00 -0400

    Added Zoom Api Integration

--- app/Http/Controllers/ZoomApiController.php
+++ app/Http/Controllers/ZoomApiController.php
@@ -50,33 +50,36 @@ class ZoomApiController extends Controller
     }
 
     function createAUser($user_email, $user_type){
-        $createAUserArray = array();
-        $createAUserArray['email'] = $user_email;
-        $createAUserArray['type'] = $user_type;
+        $createAUserArray = array(
+            'email' => $user_email,
+            'type'  => $user_type
+        );
         return $this->sendRequest('user/create', $createAUserArray);
     }
 
     function autoCreateAUser($user_email, $user_type, $user_password){
-        $autoCreateAUserArray = array();
-        $autoCreateAUserArray['email'] = $user_email;
-        $autoCreateAUserArray['type'] = $user_type;
-        $autoCreateAUserArray['password'] = $user_password;
+        $autoCreateAUserArray = array(
+            'email'     => $user_email,
+            'type'      => $user_type,
+            'password'  => $user_password
+        );
         return $this->sendRequest('user/autocreate', $autoCreateAUserArray);
     }
 
     function custCreateAUser($user_email, $user_type){
-        $custCreateAUserArray = array();
-        $custCreateAUserArray['email'] = $user_email;
-        $custCreateAUserArray['type'] = $user_type;
+        $custCreateAUserArray = array(
+            'email' => $user_email,
+            'type'  => $user_type
+        );
         return $this->sendRequest('user/custcreate', $custCreateAUserArray);
     }
 
     function deleteAUser($user_id, $page_size = 50, $page_number = 1){
         $deleteAUserArray = array(
+            'id'            => $user_id,
             'page_size'     => $page_size,
             'page_number'   => $page_number
         );
-        $deleteAUserArray['id'] = $user_id;
         return $this->sendRequest('user/delete', $deleteAUserArray);
     }
 
@@ -96,179 +99,184 @@ class ZoomApiController extends Controller
         return $this->sendRequest('user/pending', $listPendingUsersArray);
     }
 
-
     function getUserInfo($user_id){
-        $getUserInfoArray = array();
-        $getUserInfoArray['id'] = $user_id;
+        $getUserInfoArray = array(
+            'id' => $user_id
+        );
         return $this->sendRequest('user/get',$getUserInfoArray);
     }
 
-
     function getUserInfoByEmail($user_email, $user_login_type){
-        $getUserInfoByEmailArray = array();
-        $getUserInfoByEmailArray['email'] = $user_email;
-        $getUserInfoByEmailArray['login_type'] = $user_login_type;
+        $getUserInfoByEmailArray = array(
+            'email'         => $user_email,
+            'login_type'    => $user_login_type
+        );
         return $this->sendRequest('user/getbyemail',$getUserInfoByEmailArray);
     }
 
-
     function updateUserInfo($user_id){
-        $updateUserInfoArray = array();
-        $updateUserInfoArray['id'] = $user_id;
+        $updateUserInfoArray = array(
+            'id' => $user_id
+        );
         return $this->sendRequest('user/update',$updateUserInfoArray);
     }
 
-
     function updateUserPassword($user_id, $user_new_password){
-        $updateUserPasswordArray = array();
-        $updateUserPasswordArray['id'] = $user_id;
-        $updateUserPasswordArray['password'] = $user_new_password;
+        $updateUserPasswordArray = array(
+            'id'        => $user_id,
+            'password'  => $user_new_password
+        );
         return $this->sendRequest('user/updatepassword', $updateUserPasswordArray);
     }
 
     function setUserAssistant($user_id, $user_email, $assistant_email){
-        $setUserAssistantArray = array();
-        $setUserAssistantArray['id'] = $user_id;
-        $setUserAssistantArray['host_email'] = $user_email;
-        $setUserAssistantArray['assistant_email'] = $assistant_email;
+        $setUserAssistantArray = array(
+            'id'                => $user_id,
+            'host_email'        => $user_email,
+            'assistant_email'   => $assistant_email
+        );
         return $this->sendRequest('user/assistant/set', $setUserAssistantArray);
     }
 
     function deleteUserAssistant($user_id, $user_email, $assistant_email){
-        $deleteUserAssistantArray = array();
-        $deleteUserAssistantArray['id'] = $user_id;
-        $deleteUserAssistantArray['host_email'] = $user_email;
-        $deleteUserAssistantArray['assistant_email'] = $assistant_email;
+        $deleteUserAssistantArray = array(
+            'id'                => $user_id,
+            'host_email'        => $user_email,
+            'assistant_email'   => $assistant_email
+        );
         return $this->sendRequest('user/assistant/delete',$deleteUserAssistantArray);
     }
 
-
     function revokeSSOToken($user_id, $user_email){
-        $revokeSSOTokenArray = array();
-        $revokeSSOTokenArray['id'] = $user_id;
-        $revokeSSOTokenArray['email'] = $user_email;
+        $revokeSSOTokenArray = array(
+            'id'    => $user_id,
+            'email' => $user_email
+        );
         return $this->sendRequest('user/revoketoken', $revokeSSOTokenArray);
     }
 
-
     function deleteUserPermanently($user_id, $user_email){
-        $deleteUserPermanentlyArray = array();
-        $deleteUserPermanentlyArray['id'] = $user_id;
-        $deleteUserPermanentlyArray['email'] = $user_email;
+        $deleteUserPermanentlyArray = array(
+            'id'    => $user_id,
+            'email' => $user_email
+        );
         return $this->sendRequest('user/permanentdelete', $deleteUserPermanentlyArray);
     }
 
 
     function createAMeeting($user_id, $meeting_topic, $meeting_type){
-        $createAMeetingArray = array();
-        $createAMeetingArray['host_id'] = $user_id;
-        $createAMeetingArray['topic'] = $meeting_topic;
-        $createAMeetingArray['type'] = $meeting_type;
+        $createAMeetingArray = array(
+            'host_id'   => $user_id,
+            'topic'     => $meeting_topic,
+            'type'      => $meeting_type
+        );
         return $this->sendRequest('meeting/create', $createAMeetingArray);
     }
 
-
     function deleteAMeeting($meeting_id, $user_id){
-        $deleteAMeetingArray = array();
-        $deleteAMeetingArray['id'] = $meeting_id;
-        $deleteAMeetingArray['host_id'] = $user_id;
+        $deleteAMeetingArray = array(
+            'id'        => $meeting_id,
+            'host_id'   => $user_id
+        );
         return $this->sendRequest('meeting/delete', $deleteAMeetingArray);
     }
 
     function listMeetings($user_id, $page_size = 50, $page_number = 1){
         $listMeetingsArray = array(
+            'host_id'       => $user_id,
             'page_size'     => $page_size,
             'page_number'   => $page_number
         );
-        $listMeetingsArray['host_id'] = $user_id;
         return $this->sendRequest('meeting/list',$listMeetingsArray);
     }
 
-
     function getMeetingInfo($meeting_id, $user_id){
-        $getMeetingInfoArray = array();
-        $getMeetingInfoArray['id'] = $meeting_id;
-        $getMeetingInfoArray['host_id'] = $user_id;
+        $getMeetingInfoArray = array(
+            'id'        => $meeting_id,
+            'host_id'   => $user_id
+        );
         return $this->sendRequest('meeting/get', $getMeetingInfoArray);
     }
 
-
     function endAMeeting($meeting_id, $user_id){
-        $endAMeetingArray = array();
-        $endAMeetingArray['id'] = $meeting_id;
-        $endAMeetingArray['host_id'] = $user_id;
+        $endAMeetingArray = array(
+            'id'        => $meeting_id,
+            'host_id'   => $user_id
+        );
         return $this->sendRequest('meeting/end', $endAMeetingArray);
     }
 
-
     function getDailyReport($month, $year){
-        $getDailyReportArray = array();
-        $getDailyReportArray['month'] = $month;
-        $getDailyReportArray['year'] = $year;
+        $getDailyReportArray = array(
+            'month' => $month,
+            'year'  => $year
+        );
         return $this->sendRequest('report/getdailyreport', $getDailyReportArray);
     }
 
-
     function getAccountReport($from, $to){
-        $getAccountReportArray = array();
-        $getAccountReportArray['from'] = $from;
-        $getAccountReportArray['to'] = $to;
+        $getAccountReportArray = array(
+            'from'  => $from,
+            'to'    => $to
+        );
         return $this->sendRequest('report/getaccountreport', $getAccountReportArray);
     }
 
-
     function getUserReport($user_id, $from, $to){
-        $getUserReportArray = array();
-        $getUserReportArray['user_id'] = $user_id;
-        $getUserReportArray['from'] = $from;
-        $getUserReportArray['to'] = $to;
+        $getUserReportArray = array(
+            'user_id'   => $user_id,
+            'from'      => $from,
+            'to'        => $to
+        );
         return $this->sendRequest('report/getuserreport', $getUserReportArray);
     }
 
-
     function createAWebinar($user_id, $topic){
-        $createAWebinarArray = array();
-        $createAWebinarArray['host_id'] = $user_id;
-        $createAWebinarArray['topic'] = $topic;
+        $createAWebinarArray = array(
+            'host_id'   => $user_id,
+            'topic'     => $topic
+        );
         return $this->sendRequest('webinar/create',$createAWebinarArray);
     }
 
     function deleteAWebinar($webinar_id, $user_id){
-        $deleteAWebinarArray = array();
-        $deleteAWebinarArray['id'] = $webinar_id;
-        $deleteAWebinarArray['host_id'] = $user_id;
+        $deleteAWebinarArray = array(
+            'id'        => $webinar_id,
+            'host_id'   => $user_id
+        );
         return $this->sendRequest('webinar/delete',$deleteAWebinarArray);
     }
 
     function listWebinars($user_id, $page_size = 50, $page_number = 1){
         $listWebinarsArray = array(
+            'host_id'       => $user_id,
             'page_size'     => $page_size,
             'page_number'   => $page_number
         );
-        $listWebinarsArray['host_id'] = $user_id;
         return $this->sendRequest('webinar/list',$listWebinarsArray);
     }
 
     function getWebinarInfo($webinar_id, $user_id){
-        $getWebinarInfoArray = array();
-        $getWebinarInfoArray['id'] = $webinar_id;
-        $getWebinarInfoArray['host_id'] = $user_id;
+        $getWebinarInfoArray = array(
+            'id'        => $webinar_id,
+            'host_id'   => $user_id
+        );
         return $this->sendRequest('webinar/get',$getWebinarInfoArray);
     }
 
-
     function updateWebinarInfo($webinar_id, $user_id){
-        $updateWebinarInfoArray = array();
-        $updateWebinarInfoArray['id'] = $webinar_id;
-        $updateWebinarInfoArray['host_id'] = $user_id;
+        $updateWebinarInfoArray = array(
+            'id'        => $webinar_id,
+            'host_id'   => $user_id
+        );
         return $this->sendRequest('webinar/update',$updateWebinarInfoArray);
     }
-
-
+    
     function endAWebinar($webinar_id, $user_id){
-        $endAWebinarArray = array();
-        $endAWebinarArray['id'] = $webinar_id;
-        $endAWebinarArray['host_id'] = $user_id;
+        $endAWebinarArray = array(
+            'id'        => $webinar_id,
+            'host_id'   => $user_id
+        );
         return $this->sendRequest('webinar/end',$endAWebinarArray);
     }
 
