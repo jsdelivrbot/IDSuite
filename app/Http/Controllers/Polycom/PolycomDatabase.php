e114e6f316355d5caa379e190718c175b787816e
IDS\fbreidi <fbreidi@e-idsolutions.com>
2017-09-21 17:58:23 -0400

    lifesize, polycom, vidyo controllers fixed endpoint table minor bugs

---
+++ app/Http/Controllers/Polycom/PolycomDatabase.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fbreidi
+ * Date: 9/20/2017
+ * Time: 10:13 AM
+ */
+
+namespace App\Http\Controllers\Polycom;
+
+
+class PolycomDatabase
+{
+
+}
---
+++ app/Http/Controllers/Vidyo/VidyoController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fbreidi
+ * Date: 9/14/2017
+ * Time: 2:38 PM
+ */
+
+namespace App\Http\Controllers\Vidyo;
+
+
+class VidyoController
+{
+
+}
