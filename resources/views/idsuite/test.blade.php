620ad87bd0cc2a01a5013a843115566931934abc
Alex Macdonald-Smith <Alex.MacSmith@gmail.com>
2017-11-28 16:53:51 -0500

    testview updates (playground for webrtc)

--- resources/views/idsuite/test.blade.php
+++ resources/views/idsuite/test.blade.php
@@ -26,11 +26,22 @@
     </div>
 
 
+    <div>
+        <canvas id="meter" width="1730" height="50"></canvas>
+    </div>
+
+
 
 
 
 
 <script>
+
+    let meterwidth = 1730;
+
+    let meterheight = 50;
+
+    let rafID = null;
 
     let inputaudio = document.getElementById('input-audio');
 
@@ -168,11 +179,44 @@
     function gotStream(stream) {
         window.stream = stream; // make stream available to console
         videoElement.srcObject = stream;
+
+        mediaStreamSource = audioContext.createMediaStreamSource(stream);
+
+        meter = createAudioMeter(audioContext);
+        mediaStreamSource.connect(meter);
+
+        drawLoop();
+
         // Refresh button list in case labels have become available
         return navigator.mediaDevices.enumerateDevices();
     }
 
+    function drawLoop( time ) {
+        // clear the background
+        canvasContext.clearRect(0,0, meterwidth, meterheight);
+
+        // check if we're currently clipping
+        if (meter.checkClipping())
+            canvasContext.fillStyle = "red";
+        else
+            canvasContext.fillStyle = "green";
+
+        // draw a bar based on the current volume
+        canvasContext.fillRect(0, 0, meter.volume*meterwidth*20, meterheight);
+
+        // set up the next visual callback
+        rafID = window.requestAnimationFrame( drawLoop );
+    }
+
     function start() {
+
+        canvasContext = document.getElementById( "meter" ).getContext("2d");
+
+        window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+        audioContext = new AudioContext();
+
+
         if (window.stream) {
             window.stream.getTracks().forEach(function(track) {
                 track.stop();
@@ -181,11 +225,24 @@
         var audioSource = inputaudio.value;
         var videoSource = inputvideo.value;
         var constraints = {
-            video: {deviceId: videoSource ? {exact: videoSource} : undefined}
+            video: {deviceId: videoSource ? {exact: videoSource} : undefined},
+            audio: {
+                mandatory: {
+                    googEchoCancellation: false,
+                    googAutoGainControl: false,
+                    googNoiseSuppression: false,
+                    googHighpassFilter: false
+                },
+                optional: []
+            },
         };
 
         navigator.mediaDevices.getUserMedia(constraints).
-        then(gotStream).then(gotDevices).catch(handleError);
+        then(gotStream).then(function(stuff){
+            console.log(stuff);
+        }).catch(function (errorStuff) {
+            console.log(errorStuff);
+        });
     }
 
     function snapShot(){
@@ -223,6 +280,63 @@
         })
 
 
+    }
+
+    function createAudioMeter(audioContext,clipLevel,averaging,clipLag) {
+        var processor = audioContext.createScriptProcessor(512);
+        processor.onaudioprocess = volumeAudioProcess;
+        processor.clipping = false;
+        processor.lastClip = 0;
+        processor.volume = 0;
+        processor.clipLevel = clipLevel || 0.98;
+        processor.averaging = averaging || 0.95;
+        processor.clipLag = clipLag || 750;
+
+        // this will have no effect, since we don't copy the input to the output,
+        // but works around a current Chrome bug.
+        processor.connect(audioContext.destination);
+
+        processor.checkClipping =
+            function(){
+                if (!this.clipping)
+                    return false;
+                if ((this.lastClip + this.clipLag) < window.performance.now())
+                    this.clipping = false;
+                return this.clipping;
+            };
+
+        processor.shutdown =
+            function(){
+                this.disconnect();
+                this.onaudioprocess = null;
+            };
+
+        return processor;
+    }
+
+    function volumeAudioProcess( event ) {
+        var buf = event.inputBuffer.getChannelData(0);
+        var bufLength = buf.length;
+        var sum = 0;
+        var x;
+
+        // Do a root-mean-square on the samples: sum up the squares...
+        for (var i=0; i<bufLength; i++) {
+            x = buf[i];
+            if (Math.abs(x)>=this.clipLevel) {
+                this.clipping = true;
+                this.lastClip = window.performance.now();
+            }
+            sum += x * x;
+        }
+
+        // ... then take the square root of the sum.
+        var rms =  Math.sqrt(sum / bufLength);
+
+        // Now smooth this out with the averaging factor applied
+        // to the previous sample - take the max here because we
+        // want "fast attack, slow release."
+        this.volume = Math.max(rms, this.volume*this.averaging);
     }
 
     function processImage() {
