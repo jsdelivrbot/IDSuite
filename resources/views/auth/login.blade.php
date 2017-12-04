38b8a0f246028c8f57cbe09b332a58d961758a66
IDS\fbreidi <fbreidi@e-idsolutions.com>
2017-12-04 17:13:22 -0500

    updated type to mov

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -73,7 +73,7 @@
     </div>
 </div>
 <video poster="http://192.35.252.40/img/login_background.mov" id="bgvid" playsinline autoplay muted loop>
-<source src="http://192.35.252.40/img/login_background.mov" type="video/mp4">
+<source src="http://192.35.252.40/img/login_background.mov" type="video/mov">
 </video>
 
     <style>
